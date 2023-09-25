57a70beecd87cd5fe064d9e87f9c3244c82534fb
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-09-25 22:36:33 +0200

    apiAndUiUpdate

--- src/Components/DogsGrid.jsx
+++ src/Components/DogsGrid.jsx
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 const DogsGrid = () => {
-  
   const apiUrl = "https://api.jsonbin.io/v3/b/650d780d0574da7622aeb34a";
 
-  const getDogs = async () => {};
-
-  const imgUrl = (img) => {};
-
-  const [dogData, setDogData] = useState(null);
+  const [dogData, setDogData] = useState([]);
   //   const [view, setView] = useState(0);
 
   useEffect(() => {
@@ -17,29 +12,43 @@ const DogsGrid = () => {
     loadPage();
   }, []);
 
-  const updateUi = (data) => {
-    data.dogs.dog.forEach((dog) => {
-      const dogElement = document.createElement("div");
-      dogElement.setAttribute("class", dog - element);
-
-      const imgElement = document.createElement("img");
-      imgElement.setAttribute("src", ImgUrl);
-    });
-  };
+  useEffect(() => {
+    // SET DOG DATA!!!
+    // console.log("data => " + JSON.stringify(dogData, null, 2));
+    updateUi(dogData);
+  }, [dogData]);
 
   const loadPage = async () => {
     const response = await fetch(apiUrl);
     console.log("got response", response);
 
-    const dogData = await response.json
-    console.log("got Data", dogData);
+    const data = await response.json();
+    setDogData(data.record);
   };
 
-  return (
-    <div className="dog-grid">
-      <div>DogsGrid</div>
-    </div>
-  );
+  const updateUi = (data) => {
+    const dogGrid = document.getElementById("dog-grid");
+    dogGrid.innerHTML = "";
+
+    data.forEach((dog) => {
+      const dogTag = document.createElement("div");
+      dogTag.className = "dog-tag";
+
+      const imgElement = document.createElement("img");
+      imgElement.src = dog.img;
+      imgElement.alt = dog.name;
+      imgElement.className = "dog-img";
+
+      const dogName = document.createElement("h1");
+      dogName.innerHTML = dog.name;
+
+      dogTag.append(imgElement, dogName);
+
+      dogGrid.appendChild(dogTag);
+    });
+  };
+
+  return <div id="dog-grid"></div>;
 };
 
 export default DogsGrid;
