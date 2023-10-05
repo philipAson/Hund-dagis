5ec6d676ecb877f8d07d1aa3073e5527be586181
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-10-05 12:18:23 +0200

    css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div id="home">
+      <h1>Välkommen till hund-dagiset!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quod
+        accusantium quaerat earum doloribus praesentium sed velit, corporis
+        laborum. Iste esse nobis illo voluptate unde dolores, saepe molestias
+        commodi sequi?
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
